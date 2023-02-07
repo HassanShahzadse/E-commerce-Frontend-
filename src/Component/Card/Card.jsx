b63666c9e6c8c879cbc6b455986c8e00b6e77195
unknown <SysAdmin@SCIPC-008.IPC.EDU>
2023-02-07 19:11:03 +0500

    added funtionality to display data that was fetch from api

--- src/Component/Card/Card.jsx
+++ src/Component/Card/Card.jsx
@@ -1,22 +1,15 @@
-import axios from "axios";
-import React, { useEffect, useState } from "react";
-function Card() {
-    const [price, setPrice] = useState([]);
-    const [catogry, setCatogry] = useState([]);
-    const [Name, setName] = useState([]);
-    const [Img, setImg] = useState([]);
-    useEffect(() => {
-        axios.get('https://fakestoreapi.com/products')
-        .then(
-            Response=>{
-                const res= Response.data;
-                console.log(res)
-            })
-        
-    }, [])
+
+import React from "react";
+function Card(Props) {
+   
     return (
-      <div className="App">
-        <h1>hello</h1>
+        
+      <div className="cardDiv">
+        <h4>{Props.title}</h4>
+        <h4>{Props.price}</h4>
+        <h4>{Props.category}</h4>
+        <h4>{Props.description}</h4>
+        <img src={Props.image}/>
       </div>
     );
   }
--- src/Pages/Products/Products.jsx
+++ src/Pages/Products/Products.jsx
@@ -1,10 +1,32 @@
-import React from "react";
+import axios from "axios";
+import React, { useEffect, useState } from "react";
 import Card from "../../Component/Card/Card";
 function Products() {
-    
+    const [item, setItem] = useState([]);
+    useEffect(() => {
+        axios.get('https://fakestoreapi.com/products')
+        .then(
+            Response=>{
+                const res = Response.data
+                setItem(res);
+                
+            })
+    }, [])
     return (
       <div className="App">
-        <Card/>
+        {item.map((param,index)=>(
+            <Card
+            key={index}
+            id={param.id}
+            price={param.price}
+            title={param.title}
+            image={param.image}
+            category={param.category}
+            description={param.description}
+            />
+        ))}
+        <Card
+        />
       </div>
     );
   }
