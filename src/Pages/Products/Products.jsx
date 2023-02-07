a2813239973b7a4b52080fd5e835763c5def9c80
unknown <SysAdmin@SCIPC-008.IPC.EDU>
2023-02-07 20:19:54 +0500

    added search funtionality to the products page

--- src/Pages/Products/Products.jsx
+++ src/Pages/Products/Products.jsx
@@ -4,18 +4,31 @@ import Card from "../../Component/Card/Card";
 import './Products.css'
 function Products() {
     const [item, setItem] = useState([]);
+    const[search,setSearch]=useState([]);
     useEffect(() => {
         axios.get('https://fakestoreapi.com/products')
         .then(
             Response=>{
                 const res = Response.data
                 setItem(res);
+                setSearch(res);
                 
             })
     }, [])
+    const handleSearch=(e)=>{
+        const inputName=e.target.value;
+        const searchName=item.filter((param)=>{
+            return param.title.toLowerCase().includes(inputName.toLowerCase());
+        })
+        setSearch(searchName);
+    }
     return (
+        <>
+        <div>
+            <input type="text" placeholder="Search Item" onChange={handleSearch}/>
+        </div>
       <div className="itemcard">
-        {item.map((param,index)=>(
+        {search.map((param,index)=>(
             <Card
             key={index}
             id={param.id}
@@ -26,9 +39,8 @@ function Products() {
             description={param.description}
             />
         ))}
-        <Card
-        />
       </div>
+      </>
     );
   }
   
